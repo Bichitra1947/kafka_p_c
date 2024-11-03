264a9d430ce3232c1f2ed5320b4b6850a59ed7bd
bichitra <bichitramohansngha@gmail.com>
2024-11-03 14:48:07 +0530

    first commit

---
+++ src/main/java/in/kafka/bichitra/InKafkaApplication.java
@@ -0,0 +1,13 @@
+package in.kafka.bichitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InKafkaApplication.class, args);
+	}
+
+}
